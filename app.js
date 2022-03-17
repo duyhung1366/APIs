2be6e88d5d74a7fe44bbde89270692c88fae2186
= <=>
2022-03-17 12:49:46 +0700

    change file app.js

--- app.js
+++ app.js
@@ -39,65 +39,67 @@ function renderCourses(courses) {
 }
 
 //create data 
-// function createData() {
-//     var createBtn = document.getElementById('create');
+function createData() {
+    var createBtn = document.getElementById('create');
 
-//     createBtn.onclick = () => {
-//         var name = document.querySelector('input[name="name-courses"]').value;
-//         var decription = document.querySelector('input[name="decription"]').value;
+    createBtn.onclick = () => {
+        var name = document.querySelector('input[name="name-courses"]').value;
+        var decription = document.querySelector('input[name="decription"]').value;
 
-//         var dataForm = {
-//             "name-courses": name,
-//             decription: decription
-//         }
-//         handleCreate(dataForm);
+        var dataForm = {
+            "name-courses": name,
+            decription: decription
+        }
+        console.log('onclick')
+        handleCreate(dataForm);
 
-//     }
-// }
+    }
+}
 // // handle create 
-// function handleCreate(data) {
+function handleCreate(dataForm) {
 
-//     fetch(courseApi, optionMethodsFetch('POST', data))
-//         .then(response => response.json())
-//         .then(value => {
-//             // console.log(value);
-//             listCourse.innerHTML += stringHtml(value);
-//         })
-// }
-
-// delete 
-function deleteData(id){ 
-     fetch(courseApi + '/' + id, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-     })
+    fetch(courseApi, optionMethodsFetch('POST', dataForm))
         .then(response => response.json())
         .then(value => {
-            document.getElementById('course-'+ id).remove();
+            // return phần tử vừa thêm 
+            // console.log(value);
+            listCourse.innerHTML += stringHtml(value);
         })
 }
 
-//option method fetch
-// function optionMethodsFetch(methods, data) {
-//     if (methods === 'POST') {
-//         return {
-//             method: methods,
-//             headers: {
-//                 'Content-Type': 'application/json'
-//                 // 'Content-Type': 'application/x-www-form-urlencoded',
-//             },
-//             body: JSON.stringify(data)
-//         }
-//     } else {
-//         return {
-//             method: methods,
-//             headers: {
-//                 'Content-Type': 'application/json'
-//                 // 'Content-Type': 'application/x-www-form-urlencoded',
-//             },
-//         }
-//     }
+// delete 
+// function deleteData(id){ 
+//      fetch(courseApi + '/' + id, {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//             // 'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//      })
+//         .then(response => response.json())
+//         .then(value => {
+//             document.getElementById('course-'+ id).remove();
+//         })
 // }
+
+// option method fetch
+function optionMethodsFetch(methods, data) {
+    if (methods === 'POST') {
+        return {
+            method: methods,
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(data)
+        }
+    } else {
+        return {
+            method: methods,
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }
+    }
+}
